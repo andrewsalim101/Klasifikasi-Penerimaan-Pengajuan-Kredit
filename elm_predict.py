5d3ca000dee292955902678dcc0c48c18ddc22ca
unknown <andrewsalim9@gmail.com>
2024-12-01 08:27:30 +0700

    memprediksi input dari app.py

---
+++ elm_predict.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+# fungsi testing ELM
+def elm_predict(X, W, b, round_output=False):
+  """
+  X           : Data
+  W           : Bobot
+  b           : Bobot keluaran
+  round_output: Menentukan hasil berupa bilangan bulat atau tidak
+
+  Fungsi melakukan pengujian dengan metode ELM
+  Fungsi hasil prediksi
+  """
+  Hinit = X @ W.T
+  H = 1 / (1 + np.exp(-Hinit))
+
+  y = H @ b
+
+  if round_output:
+      y = int(round(y))
+
+  return y
